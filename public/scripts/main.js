9fd4c61bea24fa0ec87ab15da09f6391e9e4b86a
Benjamin Cuvilliez <72570840+BenjiPy@users.noreply.github.com>
2025-04-10 12:20:35 +0200

    Add files via upload

---
+++ public/scripts/main.js
@@ -0,0 +1,31 @@
+function includeHTML(file, targetId) {
+    fetch(file)
+        .then(response => {
+            if (!response.ok) throw new Error(`Erreur lors du chargement de ${file}`);
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(targetId).innerHTML = data;
+        })
+        .catch(error => console.error(error));
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    includeHTML("/site-vitrine/public/components/Header.html", "header");
+    includeHTML("/site-vitrine/public/components/Footer.html", "footer");
+
+    highlightActivePage();
+
+    window.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section');
+        const links = document.querySelectorAll('.nav-links a');
+
+        sections.forEach((section, index) => {
+            const rect = section.getBoundingClientRect();
+            if (rect.top <= 0 && rect.bottom >= 0) {
+                links.forEach(link => link.classList.remove('active'));
+                links[index].classList.add('active');
+            }
+        });
+    });
+});
